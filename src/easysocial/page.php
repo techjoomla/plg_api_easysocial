b3ccb2d71334d9fbe6ead2f9f113eb44936872ca
Deepa Gawade <deepa_g@techjoomla.com>
2021-06-02 14:31:13 +0530

    Easysocial plugin chore: Page - Custom field value show

--- src/easysocial/page.php
+++ src/easysocial/page.php
@@ -3,7 +3,7 @@
  * @package     Joomla.Site
  * @subpackage  Com_api-plugins
  *
- * @copyright   Copyright (C) 2009-2014 Techjoomla, Tekdi Technologies Pvt. Ltd. All rights reserved.
+ * @copyright   Copyright (C) 2009-2021 Techjoomla, Tekdi Technologies Pvt. Ltd. All rights reserved.
  * @license     GNU GPLv2 <http://www.gnu.org/licenses/old-licenses/gpl-2.0.html>
  * @link        http://techjoomla.com
  * Work derived from the original RESTful API by Techjoomla (https://github.com/techjoomla/Joomla-REST-API)
@@ -11,6 +11,9 @@
  */
 
 defined('_JEXEC') or die('Restricted access');
+
+use Joomla\CMS\Factory;
+use Joomla\CMS\Language\Text;
 
 jimport('joomla.plugin.plugin');
 jimport('joomla.html.html');
@@ -25,91 +28,97 @@ require_once JPATH_SITE . '/plugins/api/easysocial/libraries/uploadHelper.php';
 class EasysocialApiResourcePage extends ApiResource
 {
 	/**
-	 * Method   description
+	 * Method description
 	 *
-	 * @return  mixed
+	 * @return	object|boolean	in success object will return, in failure boolean
 	 *
-	 * @since   1.0
+	 * @since 1.0
 	 */
 	public function get()
 	{
 		// Init variable
-		$app = JFactory::getApplication();
-		$log_user = JFactory::getUser($this->plugin->get('user')->id);
-		$page_id = $app->input->get('id', 0, 'INT');
-		$mapp = new EasySocialApiMappingHelper;
-		$res = new stdclass;
-		$res->result = array();
-		$res->empty_message = '';
-		$page = array();
+		$app      = Factory::getApplication();
+		$log_user = Factory::getUser($this->plugin->get('user')->id);
+		$pageId   = $app->input->get('id', 0, 'INT');
+		$page[]   = FD::page($pageId);
 
-		if ($page_id)
+		// Ensure that the id provided is valid
+		if (empty($page[0]) || !$page[0]->id)
 		{
-			$page[] = FD::page($page_id);
-			$res->result = $mapp->mapItem($page, 'page', $log_user->id);
-			$this->plugin->setResponse($res);
+			ApiError::raiseError(400, Text::_('PLG_API_EASYSOCIAL_PAGE_NOT_FOUND'));
 		}
-		else
+
+		$res                = new stdclass;
+		$res->result        = array();
+		$res->empty_message = '';
+
+		$mapp        = new EasySocialApiMappingHelper;
+		$pageObject  = $mapp->mapItem($page, SOCIAL_TYPE_PAGE, $log_user->id, SOCIAL_TYPE_PAGE);
+		$fieldsModel = FD::model('Fields');
+
+		foreach ($pageObject[0]->more_info as $key => $value)
 		{
-			$this->plugin->err_code = 403;
-			$this->plugin->err_message = 'PLG_API_EASYSOCIAL_PAGE_NOT_FOUND';
-			$this->plugin->setResponse(null);
+			$updatedCustomFieldValue = $fieldsModel->getCustomFieldsValue($value->field_id ,$pageId ,SOCIAL_TYPE_PAGE);
+			$pageObject[0]->more_info[$key]->field_value = $updatedCustomFieldValue;
 		}
+
+		$res->result = $pageObject;
+		$this->plugin->setResponse($res);
 	}
 
 	/**
-	 * Method   description
+	 * Method description
 	 *
 	 * @return  mixed
 	 *
-	 * @since   1.0
+	 * @since 1.0
 	 */
 	public function post()
 	{
-		$this->plugin->setResponse(JText::_('PLG_API_EASYSOCIAL_UNSUPPORTED_POST_METHOD_MESSAGE'));
+		$this->plugin->setResponse(Text::_('PLG_API_EASYSOCIAL_UNSUPPORTED_POST_METHOD_MESSAGE'));
 	}
 
 	/**
-	 * Method   description
+	 * Method description
 	 *
-	 * @return  mixed
+	 * @return	object|boolean	in success object will return, in failure boolean
 	 *
-	 * @since   1.0
+	 * @since 1.0
 	 */
 	public function delete()
 	{
-		$app = JFactory::getApplication();
+		$app     = Factory::getApplication();
 		$page_id = $app->input->get('id', 0, 'INT');
-		$valid = 1;
-		$page = FD::page($page_id);
+		$valid   = 1;
+		$page    = FD::page($page_id);
 
 		// Call groups model to get page owner
 		$pagesModel = FD::model('groups');
-		$res = new stdclass;
+		$res		= new stdclass;
 
 		if (!$page->id || !$page_id)
 		{
-			$res->result->status = 0;
-			$res->result->message = JText::_('PLG_API_EASYSOCIAL_INVALID_PAGE_MESSAGE');
+			$res->result->status  = 0;
+			$res->result->message = Text::_('PLG_API_EASYSOCIAL_INVALID_PAGE_MESSAGE');
 			$valid = 0;
 		}
 
 		// Only allow super admins to delete pages
-		$my = FD::user($this->plugin->get('user')->id);
+		$my	=	FD::user($this->plugin->get('user')->id);
 
 		if (!$my->isSiteAdmin() && !$pagesModel->isOwner($my->id, $page_id))
 		{
-			$res->result->status = 0;
-			$res->result->message = JText::_('PLG_API_EASYSOCIAL_PAGE_ACCESS_DENIED_MESSAGE');
-			$valid = 0;
+			$res->result->status  = 0;
+			$res->result->message = Text::_('PLG_API_EASYSOCIAL_PAGE_ACCESS_DENIED_MESSAGE');
+			$valid				  =	0;
 		}
 
 		if ($valid)
 		{
 			// Try to delete the page
 			$page->delete();
-			$res->result->status = 1;
-			$res->result->message = JText::_('PLG_API_EASYSOCIAL_PAGE_DELETED_MESSAGE');
+			$res->result->status  = 1;
+			$res->result->message = Text::_('PLG_API_EASYSOCIAL_PAGE_DELETED_MESSAGE');
 		}
 
 		$this->plugin->setResponse($res);
--- src/easysocial/profile.php
+++ src/easysocial/profile.php
@@ -3,7 +3,7 @@
  * @package     Joomla.Site
  * @subpackage  Com_api-plugins
  *
- * @copyright   Copyright (C) 2009-2014 Techjoomla, Tekdi Technologies Pvt. Ltd. All rights reserved.
+ * @copyright   Copyright (C) 2009-2021 Techjoomla, Tekdi Technologies Pvt. Ltd. All rights reserved.
  * @license     GNU GPLv2 <http://www.gnu.org/licenses/old-licenses/gpl-2.0.html>
  * @link        http://techjoomla.com
  * Work derived from the original RESTful API by Techjoomla (https://github.com/techjoomla/Joomla-REST-API)
@@ -11,6 +11,9 @@
  */
 
 defined('_JEXEC') or die('Restricted access');
+
+use Joomla\CMS\Factory;
+use Joomla\CMS\Language\Text;
 
 jimport('joomla.plugin.plugin');
 jimport('joomla.html.html');
@@ -52,7 +55,7 @@ class EasysocialApiResourceProfile extends ApiResource
 	public function post()
 	{
 		$this->plugin->err_code = 405;
-		$this->plugin->err_message = JText::_('PLG_API_EASYSOCIAL_USE_GET_METHOD_MESSAGE');
+		$this->plugin->err_message = Text::_('PLG_API_EASYSOCIAL_USE_GET_METHOD_MESSAGE');
 		$this->plugin->setResponse(null);
 	}
 
@@ -66,24 +69,25 @@ class EasysocialApiResourceProfile extends ApiResource
 	public function getProfile()
 	{
 		// Init variable
-		$app = JFactory::getApplication();
-		$log_user = $this->plugin->get('user')->id;
-		$other_user_id = $app->input->get('user_id', 0, 'INT');
-		$userid = ($other_user_id) ? $other_user_id : $log_user;
+		$app			= Factory::getApplication();
+		$log_user		= $this->plugin->get('user')->id;
+		$other_user_id	= $app->input->get('user_id', 0, 'INT');
+		$userid			= ($other_user_id)?$other_user_id:$log_user;
 
 		$res = new stdClass;
-		$user = FD::user($userid);
+		$data =	array();
+		$user =	FD::user($userid);
 
 		if ($user->id == 0)
 		{
-			$this->plugin->err_code = 404;
-			$this->plugin->err_message = JText::_('COM_USERS_USER_NOT_FOUND');
+			$this->plugin->err_code    = 404;
+			$this->plugin->err_message = Text::_('COM_USERS_USER_NOT_FOUND');
 			$this->plugin->setResponse(null);
 		}
 
 		// Easysocial default profile
 		$profile = $user->getProfile();
-		$mapp = new EasySocialApiMappingHelper;
+		$mapp    = new EasySocialApiMappingHelper;
 
 		if ($userid)
 		{
@@ -103,15 +107,16 @@ class EasysocialApiResourceProfile extends ApiResource
 			foreach ($steps as $step)
 			{
 				$step->fields = $fieldsModel->getCustomFields(
-																	array(
-																		'step_id' => $step->id,
-																		'data' => true,
-																		'dataId' => $userid,
-																		'dataType' => SOCIAL_TYPE_USER,
-																		'visible' => SOCIAL_PROFILES_VIEW_DISPLAY
-																		)
-																);
-				$fields = null;
+					array(
+						'step_id' => $step->id,
+						'data' => true,
+						'dataId' => $userid,
+						'dataType' => SOCIAL_TYPE_USER,
+						'visible' => SOCIAL_PROFILES_VIEW_DISPLAY
+						)
+				);
+
+				$fields	=	null;
 
 				if (count($step->fields))
 				{
@@ -131,7 +136,8 @@ class EasysocialApiResourceProfile extends ApiResource
 				}
 			}
 
-			$friendsObj = ES::friends($other_user_id, $log_user);
+			$friendmodel = FD::model('Friends');
+			$friendsObj	=	ES::friends($other_user_id, $log_user);
 			$user_obj->isrequestor = $friendsObj->isRequester();
 			$user_obj->more_info[] = $field_arr;
 		}
